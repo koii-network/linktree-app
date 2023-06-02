87d83af4fb61bbcdb667136175f94a0ffa750f6e
Fafemi Adeola <adeola5678@gmail.com>
2023-06-02 20:59:35 +0100

    fix: ipfs link

--- src/CreateLinktree.js
+++ src/CreateLinktree.js
@@ -22,7 +22,8 @@ import { useWalletContext } from "./contexts";
 
 function makeStorageClient() {
   return new Web3Storage({
-    token: process.env.REACT_APP_WEB3STORAGE_TOKEN,
+    token:
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDhmOWMxOTNjODJlODMzMjVDMThkNWM4NzRCM2Q2NGM5ZjI5NDdEOUQiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2ODM2NTY1NzExNjEsIm5hbWUiOiJLb2lpIn0.qZJmInvmwLCkq_7T3h2gfm4Hs84MNKEVooOuAFfbIXI",
   });
 }
 
@@ -120,7 +121,7 @@ const CreateLinktree = () => {
             uuid: uuid(),
             linktree: {
               ...values,
-              image: `https://${imageCID}.ipfs.dweb.link/data.json/${imageName}`,
+              image: `https://${imageCID}.ipfs.dweb.link/${imageName}`,
             },
             timestamp: Date.now(),
           };
