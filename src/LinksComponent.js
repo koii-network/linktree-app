8e3eb96a9bb2ba47424059ddfe0fdadcb84c8d1c
Fafemi Adeola <adeola5678@gmail.com>
2023-06-02 23:14:12 +0100

    fix: linktree page

--- src/LinksComponent.js
+++ src/LinksComponent.js
@@ -3,6 +3,7 @@ import { useLocation } from "react-router-dom";
 import { getLinktree } from "./api";
 import { useDisconnect } from "wagmi";
 import { Oval } from "react-loader-spinner";
+import { Box } from "@chakra-ui/react";
 
 function LinksComponent() {
   const location = useLocation();
@@ -44,18 +45,20 @@ function LinksComponent() {
           </a>
         </Message>
       ) : (
-        <>
+        <Box minHeight='70vh'>
           {" "}
+          {userData?.image && (
+            <img
+              src={userData?.image}
+              alt={userData?.name}
+              className='user-image'
+            />
+          )}
           {/* <img
-            src={userData?.image}
-            alt={userData?.name}
-            className="user-image"
-          /> */}
-          <img
             src={"/images/twitter_LCtIAZQN_400x400 1.png"}
             alt={userData?.name}
             className='user-image'
-          />
+          /> */}
           <p className='user-name'> {userData?.name} </p>
           <p className='user-desc'> {userData?.description} </p>
           <div className='links'>
@@ -71,7 +74,7 @@ function LinksComponent() {
               </a>
             ))}
           </div>
-        </>
+        </Box>
       )}
       <div className='footer'>
         Link tree by{" "}
