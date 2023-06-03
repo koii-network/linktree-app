b534f215eff847a3d7ef72c7b861f694d4ad39bb
Fafemi Adeola <adeola5678@gmail.com>
2023-06-03 14:16:06 +0100

    feat: remove token

--- src/CreateLinktree.js
+++ src/CreateLinktree.js
@@ -22,8 +22,7 @@ import { useWalletContext } from "./contexts";
 
 function makeStorageClient() {
   return new Web3Storage({
-    token:
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDhmOWMxOTNjODJlODMzMjVDMThkNWM4NzRCM2Q2NGM5ZjI5NDdEOUQiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2ODM2NTY1NzExNjEsIm5hbWUiOiJLb2lpIn0.qZJmInvmwLCkq_7T3h2gfm4Hs84MNKEVooOuAFfbIXI",
+    token: process.env.REACT_APP_WEB3STORAGE_TOKEN,
   });
 }
 
