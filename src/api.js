8b777b8d3e7f7091a4bc976766e980d48dd95a82
saim <saim@koii.network>
2023-06-07 13:46:18 -0300

    added node endpoints

--- src/api.js
+++ src/api.js
@@ -9,7 +9,7 @@ import {
 
 export async function getLinktrees(publicKey) {
   try {
-    const res = await axios.get("http://localhost:10000/linktree/list");
+    const res = await axios.get("https://tasknet.koii.live/task/6FgtEX6qd4XCuycUfJGuJTr41qcfvM59ueV2L17eSdan/linktree/list");
     const profile = res.data.filter((item) => {
       return item.publicKey === publicKey;
     });
@@ -29,7 +29,7 @@ export async function getLinktrees(publicKey) {
 export async function getLinktree(publicKey) {
   try {
     const res = await axios.get(
-      `http://localhost:10000/linktree/get/${publicKey}`
+      `https://tasknet.koii.live/task/6FgtEX6qd4XCuycUfJGuJTr41qcfvM59ueV2L17eSdan//linktree/get/${publicKey}`
     );
     return res;
   } catch (error) {
@@ -46,7 +46,7 @@ export async function setLinktree(data, publicKey) {
     signature: bs58.encode(signatureRaw.signature),
   };
   try {
-    const res = await axios.post("http://localhost:10000/linktree", {
+    const res = await axios.post("https://tasknet.koii.live/task/6FgtEX6qd4XCuycUfJGuJTr41qcfvM59ueV2L17eSdan/linktree", {
       payload,
     });
     return res.data;
@@ -58,7 +58,7 @@ export async function setLinktree(data, publicKey) {
 export async function getAuthList(publicKey) {
   try {
     const res = await axios.get(
-      `http://localhost:10000/authlist/get/${publicKey}`
+      `https://tasknet.koii.live/task/6FgtEX6qd4XCuycUfJGuJTr41qcfvM59ueV2L17eSdan//authlist/get/${publicKey}`
     );
     return res?.data === publicKey;
   } catch (error) {
@@ -91,7 +91,7 @@ export async function transferKoii() {
     const authdata = {
       pubkey: window.k2.publicKey,
     };
-    await axios.post("http://localhost:10000/authlist", {
+    await axios.post("https://tasknet.koii.live/task/6FgtEX6qd4XCuycUfJGuJTr41qcfvM59ueV2L17eSdan/authlist", {
       authdata,
     });
   } catch (error) {
