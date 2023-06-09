037c46f89369864e25a62a4631b488662b09c1f9
Fafemi Adeola <adeola5678@gmail.com>
2023-06-10 00:30:04 +0100

    fix: recipient address

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 // Other constants
 export const DOWNLOAD_FINNIE_URL =
   "https://chrome.google.com/webstore/detail/finnie/cjmkndjhnagcfbpiemnkdpomccnjblmj";
-export const RECIPIENT_ADDRESS = "HdVLjiwWcX8RWCLgZnNzeMt48JB6MJYan7q2qt3NLfZW";
+export const RECIPIENT_ADDRESS = "stakepotaccount2YjJnz34eyunRGBNrAFdMM4Rmwop";
 export const Transfer_AMOUNT = 10000000000;
 export const TASK_NODES_URL =
   "https://tasknet.koii.live/nodes/6N5s2YwMZfUQjjuS3z2JDKLkJczZDQDrEQtWYZrbVRQJ/";
