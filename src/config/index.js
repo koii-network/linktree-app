4e3648dc9b06a3e1dfbb36cdcd8e522b98457713
Fafemi Adeola <adeola5678@gmail.com>
2023-06-15 15:51:24 +0100

    fix: change task address to old

--- src/config/index.js
+++ src/config/index.js
@@ -4,6 +4,5 @@ export const DOWNLOAD_FINNIE_URL =
 export const RECIPIENT_ADDRESS = "stakepotaccountuQLBn4bsxKgSLedRTxsnZUQ9aCBR";
 export const Transfer_AMOUNT = 10000000000;
 export const TASK_NODES_URL =
-  "https://tasknet.koii.live/nodes/DyzjHaJe4KVUQJBCrW1bgmXB79ovAjSQGDYDKV8BiRL5/";
-export const TASK_ADDRESS = "DyzjHaJe4KVUQJBCrW1bgmXB79ovAjSQGDYDKV8BiRL5";
-
+  "https://tasknet.koii.live/nodes/6N5s2YwMZfUQjjuS3z2JDKLkJczZDQDrEQtWYZrbVRQJ/";
+export const TASK_ADDRESS = "6N5s2YwMZfUQjjuS3z2JDKLkJczZDQDrEQtWYZrbVRQJ";
