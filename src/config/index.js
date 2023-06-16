0524d32a7bf46f6c82dcf245fa50254a4b56f467
Yigit Dincsoy <w0471994@campus.nscc.ca>
2023-06-16 16:21:39 -0300

    Updated TASK_NODES_URL

--- src/config/index.js
+++ src/config/index.js
@@ -4,5 +4,5 @@ export const DOWNLOAD_FINNIE_URL =
 export const RECIPIENT_ADDRESS = "stakepotaccountuQLBn4bsxKgSLedRTxsnZUQ9aCBR";
 export const Transfer_AMOUNT = 10000000000;
 export const TASK_NODES_URL =
-  "https://tasknet.koii.live/nodes/6N5s2YwMZfUQjjuS3z2JDKLkJczZDQDrEQtWYZrbVRQJ/";
+  "https://tasknet.koii.live/nodes/B5YoRFNLaxAeczcN9cu1nbwgoTme5ngTdEjQ2KJvxuH4/";
 export const TASK_ADDRESS = "B5YoRFNLaxAeczcN9cu1nbwgoTme5ngTdEjQ2KJvxuH4";
