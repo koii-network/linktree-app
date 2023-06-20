8af734fc248da25437a02b467f3df9e40c9d3efb
saim <saim@koii.network>
2023-06-20 10:40:36 -0300

    new tsk

--- src/config/index.js
+++ src/config/index.js
@@ -1,11 +1,12 @@
 // Other constants
 export const DOWNLOAD_FINNIE_URL =
   "https://chrome.google.com/webstore/detail/finnie/cjmkndjhnagcfbpiemnkdpomccnjblmj";
-export const RECIPIENT_ADDRESS = "stakepotaccountuQLBn4bsxKgSLedRTxsnZUQ9aCBR";
+export const RECIPIENT_ADDRESS = "stakepotaccountU93ZtELQRDvq7k6JjTg1hof78oBZ";
 export const Transfer_AMOUNT = 10000000000;
 export const TASK_NODES_URL =
-  "https://tasknet.koii.live/nodes/BWuGQi8e6pEWKszQey5SxXhwMT3TPDT7FQs21Ga4db4v/";
-export const TASK_ADDRESS = "BWuGQi8e6pEWKszQey5SxXhwMT3TPDT7FQs21Ga4db4v";
+  "https://tasknet.koii.live/nodes/CxhjPZaBQ12eN3N8qv3pACPuAZmWHPZSqYPAcywkhzNX/";
+export const TASK_ADDRESS = "CxhjPZaBQ12eN3N8qv3pACPuAZmWHPZSqYPAcywkhzNX";
 
 
-//B5YoRFNLaxAeczcN9cu1nbwgoTme5ngTdEjQ2KJvxuH4
+
+//CxhjPZaBQ12eN3N8qv3pACPuAZmWHPZSqYPAcywkhzNX
