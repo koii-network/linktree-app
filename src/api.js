c8682dae84ea1aae924564911f2d7ede1b327daa
Fafemi Adeola <adeola5678@gmail.com>
2023-07-07 18:27:11 +0100

    fix: transfer koii api call

--- src/api.js
+++ src/api.js
@@ -1,6 +1,12 @@
 import axios from "axios";
 import bs58 from "bs58";
-import { TASK_ADDRESS } from "./config";
+import { TASK_ADDRESS, RECIPIENT_ADDRESS, TRANSFER_AMOUNT } from "./config";
+import {
+  SystemProgram,
+  Transaction,
+  Connection,
+  clusterApiUrl,
+} from "@_koi/web3.js";
 
 export async function deleteLinktree(nodeList, publicKey) {
   try {
@@ -133,6 +139,7 @@ export async function getLinktree(publicKey, nodeList) {
 export async function setLinktree(data, publicKey, nodeList, username) {
   const messageString = JSON.stringify(data);
   try {
+    await transferKoii(nodeList);
     const signatureRaw = await window.k2.signMessage(messageString);
     const payload = {
       data,
@@ -188,6 +195,82 @@ export async function updateLinktree(data, publicKey, nodeList, username) {
 
     if (result?.message) {
       return result;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function getAuthList(publicKey, nodeList) {
+  try {
+    const requests = nodeList.map((node) =>
+      axios
+        .get(`${node}/task/${TASK_ADDRESS}/authlist/get/${publicKey}`)
+        .then((res) => res.data)
+        .catch((error) =>
+          console.log(`Error fetching authlist from ${node}:`, error)
+        )
+    );
+
+    const results = await Promise.allSettled(requests);
+
+    for (const result of results) {
+      if (result.status === "fulfilled" && result.value === publicKey) {
+        return true;
+      }
+    }
+    return false;
+  } catch (error) {
+    console.log("Error getting node list:", error);
+  }
+}
+
+export async function transferKoii(nodeList) {
+  try {
+    const connection = new Connection(clusterApiUrl("devnet"));
+    const blockHash = await connection.getRecentBlockhash();
+    const feePayer = window.k2.publicKey;
+
+    const transaction = new Transaction();
+    transaction.recentBlockhash = blockHash.blockhash;
+    transaction.feePayer = feePayer;
+
+    transaction.add(
+      SystemProgram.transfer({
+        fromPubkey: window.k2.publicKey,
+        toPubkey: new window.solanaWeb3.PublicKey(RECIPIENT_ADDRESS),
+        lamports: Number(
+          TRANSFER_AMOUNT +
+            (await connection.getMinimumBalanceForRentExemption(100)) +
+            1000
+        ),
+      })
+    );
+
+    const payload = transaction.serializeMessage();
+    const signature = await window.k2.signAndSendTransaction(payload);
+
+    if (signature) {
+      const authdata = {
+        pubkey: window.k2.publicKey.toString(),
+      };
+      let nodeListIndex = 1;
+      let result;
+      while (!result) {
+        result = await axios
+          .post(`${nodeList[nodeListIndex]}/task/${TASK_ADDRESS}/authlist`, {
+            authdata,
+          })
+          .then((res) => res.data === window.k2.publicKey.toString())
+          .catch((error) =>
+            console.log(
+              `Error fetching authlist from ${nodeList[nodeListIndex]}:`,
+              error
+            )
+          );
+        nodeListIndex++;
+        if (result) return result;
+      }
     }
   } catch (error) {
     console.log(error);
--- src/config/index.js
+++ src/config/index.js
@@ -2,7 +2,7 @@
 export const DOWNLOAD_FINNIE_URL =
   "https://chrome.google.com/webstore/detail/finnie/cjmkndjhnagcfbpiemnkdpomccnjblmj";
 export const RECIPIENT_ADDRESS = "stakepotaccountU93ZtELQRDvq7k6JjTg1hof78oBZ";
-export const Transfer_AMOUNT = 10000000000;
+export const TRANSFER_AMOUNT = 10000000000;
 export const TASK_NODES_URL =
   "https://tasknet.koii.live/nodes/A2J2vyYqzuXBCftRMfwx5qcq5JGE9632FzceTvC2suZ9/";
 export const TASK_ADDRESS = "A2J2vyYqzuXBCftRMfwx5qcq5JGE9632FzceTvC2suZ9";
