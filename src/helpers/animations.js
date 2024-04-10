91f2d96acbe0a944ea99f7556784d59337bdac91
Pablo Poggio <pablopoggiog@gmail.com>
2024-04-10 16:12:17 -0300

    Update main page's cards

--- src/helpers/animations.js
+++ src/helpers/animations.js
@@ -1,5 +1,5 @@
 export function animatedSection() {
-  let images = ["/images/o3_saim.png", "/images/o1_al.png"];
+  let images = ["/images/o1_al.png"];
   let currentIndex = 0;
   document.head.appendChild(document.createElement("style")).innerHTML =
     "#animated-image-frame { animation: rotateAnimation 5s infinite linear; animation-delay: 0s; }";
@@ -15,7 +15,6 @@ export function animatedSection() {
       if (!imgElement) return;
 
       imgElement.src = images[currentIndex % images.length];
-      currentIndex++;
       setTimeout(swapImage, 2500);
     }
 
